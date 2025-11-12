cd0b8d7c205bf7ee3de3af8e594ccf27e86b943a
kehlbachDevLab <kehlbachDev@gmail.com>
2025-11-12 10:15:02 +0100

    erase for new update

--- src/view/components/SeparatorElement.tsx
+++
@@ -1,6 +0,0 @@
-// --- Demo version. Code concealed ---
-
-import { useFormatController } from '@/controller/FormatController';
-    const formatStyles = useFormatController().components.separator;  
-                opacity: Settings.opacity.global
-}// --- End of demo block ---
